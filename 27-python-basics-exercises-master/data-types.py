60639dbe51a5833c2b59739d6f1d465e09024a2a
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-06-27 23:58:11 -0400

    adds python basics exercises

---
+++ 27-python-basics-exercises-master/data-types.py
@@ -0,0 +1,51 @@
+# Starting with the following list...
+planeteers = ["Earth", "Wind", "Captain Planet", "Fire", "Water"]
+
+# Access the second value in `planeteers`
+
+
+# Add "Heart" to the end of `planeteers`
+
+
+# Remove "Captain Planet" from the list
+
+
+# Combine `planeteers` with `rangers = ["Red", "Blue", "Pink", "Yellow", "Black"]` and save the result in a variable called `heroes`
+
+
+# Alphabetize the contents of `heroes` using a function
+
+
+# Randomize the contents of `heroes` using a function. 
+# The Python documentation might help: https://docs.python.org/3/library/random.html
+
+
+# Bonus: Select a random element from `heroes` using a function
+# The Python documentation might help: https://docs.python.org/3/library/random.html
+
+
+# Initialize a dictionary called `ninja_turtle` with the properties `name`, `weapon` and `radical`
+# They should have values of "Michelangelo", "Nunchuks" and a true boolean, respectively
+
+
+# Add a key `age` to `ninja_turtle`. Set it to whatever numerical value you'd like
+
+
+# Remove the `radical` key-value pair from `ninja_turtle`
+
+
+# Add a key `pizza_toppings` to `ninja_turtle`. Set it to an list of strings (e.g., `["cheese", "pepperoni", "peppers"]`)
+
+
+# Access the first element of `pizza_toppings`
+
+
+# Produce an list containing all of `ninja_turtle`'s keys using a function. 
+# The Python documentation might help: https://docs.python.org/3/tutorial/datastructures.html
+
+
+# Produce a list containing all of `ninja_turtle`'s values using a function
+
+
+# Bonus: Write a function that prints out each key-value pair in the following format - "KEY is equal to VALUE"
+#  The Python documentation might help: https://docs.python.org/3/tutorial/datastructures.html
